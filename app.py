66588d0e0264402dde10cf79b8a475406a5c0478
1337r00t <u9cc85@gmail.com>
2019-08-06 22:48:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ oauth_token_secret "" # Your OAuth Token secret
 # API : http://seriot.ch/resources/abusing_twitter_api/twitter_api.pdf
 # http://spotidoc.com/doc/715113/visual-documentation-of-twitter-api
 # Nicolas Seriot @ Project Manager ST Twitter .
-tweets = lambda username: requests.get('https://api.twitter.com/2/timeline/profile/'+requests.get('https://help.twitter.com/api/v1/username_lookups?username='+username).json()['user_id']+'.json?count=32767',auth=requests_oauthlib.OAuth1('3nVuSoBZnx6U4vzUxf5w','Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys',oauth_token,oauth_token_secret,decoding=None)).text
+tweets = lambda username: requests.get('https://api.twitter.com/2/timeline/profile/'+requests.get('https://help.twitter.com/api/v1/username_lookups?username='+username).json()['user_id']+'.json?count=32767&include_tweet_replies=false&pc=true&earned=true&autoplay_enabled=true&include_entities=true&include_cards=true&cards_platform=Android-12&include_carousels=true&ext=stickerInfo%2CmediaRestrictions%2CaltText%2CmediaStats%2CmediaColor%2Cinfo360%2CcameraMoment%2Cmaster_playlist_only&include_media_features=true&include_blocking=true&include_blocked_by=true&tweet_mode=extended&include_reply_count=true&include_composer_source=true&simple_quoted_tweet=true&include_ext_media_availability=true&include_user_entities=true&include_profile_interstitial_type=true',auth=requests_oauthlib.OAuth1('3nVuSoBZnx6U4vzUxf5w','Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys',oauth_token,oauth_token_secret,decoding=None)).text
 
 @app.route('/')
 def index():
